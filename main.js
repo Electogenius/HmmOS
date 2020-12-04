da8129a492e8fd1a8d01651787f656107774f06a
Electogenius <69585636+Electogenius@users.noreply.github.com>
2020-12-04 18:24:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,67 @@
+//file system
+var storage = {
+  "private": {
+    'version.str': "beta 2",
+  },
+  "stored": {
+    "text.str": "true",
+    "haha.ra": ['hi', 'wow'],
+  },
+  'wow.txt': 'clap clap wow'
+}
+var node = document.createElement('window');
 //the boring variable thingies
 const errtxt = document.getElementById("popup-text");
 const blur = document.getElementById('darken');
 const windowContainer = document.getElementById('windows');
-
-var appnames = ['test', 'test2'];
+var active = ''
 var appcodes = {
-  0: ["button", 'Click me', 'script', "document.querySelector('window button').onclick = function(){alert('you got hacked LOL'); document.querySelector('*').innerHTML = 'hi'};"],
+  'test': ["button", 'Click me', 'h1', 'wwwwwwwww', 'script', "document.querySelector('window button').onclick = function(){alert('you got hacked LOL'); document.querySelector('*').innerHTML = 'hi'};"],
+  "fileExplorer": ['div', '', 'style', `
+  fefile{
+    display: block;
+    font-size: 15px;
+    background-color: #333;
+  }
+  fefolder{
+    display: block;
+    font-size: 15px;
+    background-color: #fa0;
+    border-bottom: 1px solid #000;
+  }
+  `,
+  'script',
+  `
+  var feDirectory = storage
+  var feContainer = document.getElementById('0offileExplorer');
+  function feGetFilesaddElements(){
+  for (fefile in feDirectory) {
+    var fecurrentfile = feDirectory[fefile]
+    //check if folder or file
+
+    if (typeof fecurrentfile == "string"){
+      appendNewElement('fefile', fefile, feContainer);
+
+    }else{
+      appendNewElement('fefolder', fefile, feContainer);
+    }
+  }
+}
+feGetFilesaddElements();
+$('fefolder').click(function(){
+  feContainer.innerHTML = ''
+  feDirectory = feDirectory[this.innerHTML];
+  feGetFilesaddElements();
+})
+$('fefile').click(function(){
+  alert(feDirectory[this.innerHTML]);
+})
+  `
+],
 }
 var elmnt;
-var active = 'window #1';
 var windowsOpened = 0;
+var xOffset, yOffset;
 //function defining
 function toggleblur() {
   $(".darken").toggle();
@@ -24,14 +76,45 @@ function openWindow(appID){
   //get the code and stuff
   var classCount = 0;
   var windowcode = appcodes[appID];
-  var windowname = appnames[appID];
+  var windowname = appID;
   var node = document.createElement("window"); //make a node (for the app)
   //decode the windowcode
-  var taskbar = document.createTextNode(windowname + '\n');
+  //title bar (named taskbar)
+  const position = { x: 0, y: 0 }
+
+interact(node).draggable({
+      allowFrom: 'taskbar',
+      modifiers: [
+
+      ],
+      listeners: {
+        start (event) {
+
+        },
+        move (event) {
+          position.x += event.dx
+          position.y += event.dy
+
+          event.target.style.transform =
+            `translate(${position.x}px, ${position.y}px)`
+        },
+      }
+    })
+
+  var taskbar = document.createTextNode('' + windowname + '');
   var taskbardiv = document.createElement('taskbar')
-  taskbardiv.id = 'window #' + windowsOpened + "header";
+  taskbardiv.id = 'windowId' + windowsOpened + "header";
   taskbardiv.appendChild(taskbar);
+  taskbardiv.class = "windowTitle" + windowsOpened;
   node.appendChild(taskbardiv)
+  //close
+  taskbar = document.createTextNode('X');
+  taskbardiv = document.createElement('close');
+  taskbardiv.appendChild(taskbar);
+  taskbardiv.id = "closeButton" + windowsOpened;
+
+  node.appendChild(taskbardiv);
+  //aaa
   var line;
   for (i = 0; i < windowcode.length; i++){
     line = windowcode[i];
@@ -48,59 +131,22 @@ function openWindow(appID){
 
   };//-for
   windowsOpened++;
-  node.id = 'window #' + windowsOpened;
+  node.id = 'windowId' + windowsOpened;
   document.getElementById('windows').appendChild(node); // add to window container
-  dragElement(document.getElementById(active));
+
   elmnt = document.getElementById(active);
 }//-function
-
-
-//setup idk
-//dragElement(document.getElementById("window #1"));
-//
-function dragElement(elmnt) {
-  elmnt = elmnt;
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+function appendNewElement(ae_name, ae_content, ae_element) {
+  var ae_node = newElement(ae_name, ae_content)
+  ae_element.appendChild(ae_node)
 }
-function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+function newElement(ne_tagname, ne_content) {
+  var ne_textnode = document.createTextNode(ne_content)
+  var ne_node = document.createElement(ne_tagname)
+  ne_node.appendChild(ne_textnode)
+  return ne_node;
 }
-function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-  function closeDragElement() {
-      // stop moving when mouse button is released:
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
-//aaaa
-$(document).on('mousedown', 'window', function(){
+$(document).on("click", 'taskbar', function(){
   active = this.id;
-  //dragElement(document.getElementById(activeWindow));
 
-  elmnt = document.getElementById(active);
 });
-//
