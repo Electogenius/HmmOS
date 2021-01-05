0b096c52a5538ab0b63adb6607697bb918f5beee
Ectogen <sndeepak@icloud.com>
2021-01-05 13:26:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -310,7 +310,10 @@ if (document.referrer.startsWith('https://liimee.github.io') || document.referre
 	alert("you're already in the best OS, and no HmmOS is not better. And if you came here from bit.ly or vercel.app, NOPE LOL")
 	window.history.back();
 }
-
+//rickroll for iframes
+if(window.location !== window.top){
+   $('body').innerHTML = '<iframe width="420" height="345" src="http://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1" frameborder="0" allowfullscreen></iframe>'
+}
 //modern app maker
 function openApp(code, name) {
   var openwin = new appWindow(name, code);
