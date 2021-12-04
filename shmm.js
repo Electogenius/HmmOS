86823a51a967fb20068e0de631a6fc12122f0734
Electogenius <69585636+Electogenius@users.noreply.github.com>
2021-12-04 16:41:22 +0530

    more progress with shell

--- shmm.js
+++ shmm.js
@@ -5,27 +5,75 @@
 		, path = true
 		, cwd = "/"
 		, hmm = window.parent.hmm
-		,v ={cmd:""}
+		, v = { cmd: "" }
+		, lines = []
+	const ti = {
+		echo(t) {
+			tx = 0
+			lines.push({
+				t,
+				fg:'#fff',
+				bg:'#000',
+				x:tx,
+				y:ty
+			})
+		},
+		err(t) {
+			tx = 0
+			lines.push({
+				t,
+				fg:'#f00',
+				bg:'#000',
+				x:tx,
+				y:ty
+			})
+		}
+	}
+	function r(o) {
+		rText(o.t, o.fg, o.bg, o.x, o.y)
+	}
 	requestAnimationFrame(function draw() {
 		clear()
 		if (path) {
-			tx=0
-			rText(cwd + " $", "#48f", "#000", tx, ty)
-			tx=cwd.length+3
-			rText(v.cmd,"#FFF","#000",tx,ty)
-			tx+=v.cmd.length
-			render(" ","#000","#fff",tx,ty)//cursor
+			tx = 0
+			lines[0]=({
+				t: cwd + " $",
+				fg: "#48f",
+				bg: "#000",
+				x: tx,
+				y: ty
+			})
+			tx = cwd.length + 3
+			lines[1]=({
+				t: v.cmd,
+				fg: "#fff",
+				bg: "#000",
+				x: tx,
+				y: ty
+			})
+			tx += v.cmd.length
+			lines[2]=({
+				t: " ",
+				fg: "#000",
+				bg: "#fff",
+				x: tx,
+				y: ty
+			})//cursor
 		}
+		lines.forEach(r)
 		requestAnimationFrame(draw)
 	})
-	window.onkeydown=(ev)=>{
-		if(path){
-			if(ev.code=="Backspace"){
-				v.cmd=v.cmd.slice(0,-1)
-			}else if(ev.code="Enter"){
-				
-			}else{
-				v.cmd+=(ev.key.length==1)?ev.key:''
+	window.onkeydown = (ev) => {
+		if (path) {
+			if (ev.code == "Backspace") {
+				v.cmd = v.cmd.slice(0, -1)
+			} else if (ev.code == "Enter") {
+				path = false
+				lines=[]
+				hmm.$(v.cmd,ti)
+				v.cmd = ''
+			} else {
+				v.cmd += (ev.key.length == 1) ? ev.key : ''
 			}
 		}
 	}
