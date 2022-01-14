d7408a13d27bb27a87f999c99827b4861a81de8b
Electogenius <69585636+Electogenius@users.noreply.github.com>
2022-01-14 23:06:17 +0530

    DUM INPUT DUDNT WANNA CHANGE ITS FONT AAAAAA

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function e(query) {
 }
 window.hmm = {
 	testcommand: function () { //runs when in development
-		hmm.openApp("store.hmm")
+		hmm.openApp("textpad.hmm")
 	},
 	restart: function () { //refresh page
 		window.location = window.location.href
